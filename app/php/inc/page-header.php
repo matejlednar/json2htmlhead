1393c63e6d4b91a9835cf4e320d47e49cdad4384
Matej Lednár <github@mldgroup.com>
2017-01-22 14:35:25 +0100

    docs update

--- app/php/inc/page-header.php
+++ app/php/inc/page-header.php
@@ -1,4 +1,12 @@
 <?
+/* 
+ * JSON to HTML <head> for PHP (json2htmlhead)
+ * 
+ * Author: PhDr. Matej Lednár, PhD.
+ * Website: work.mldgroup.com
+ * GitHub: https://github.com/matejlednar/json2htmlhead
+ * 
+ */
 include_once 'app/php/classes/header-generator.php';
 
 $hg = new HeaderGenerator();
